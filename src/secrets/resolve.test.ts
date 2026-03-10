182e38f1f046922f0954b10d50943657b7899e9d
SC-Claw <sc-claw@SC-ClawdeMac-mini.local>
2026-03-10 10:34:21 +0800

    Secrets: narrow Windows ACL bypass

--- src/secrets/resolve.test.ts
+++ src/secrets/resolve.test.ts
@@ -40,6 +40,7 @@ describe("secret ref resolver", () => {
     command: string;
     passEnv?: string[];
     jsonOnly?: boolean;
+    allowInsecurePath?: boolean;
     allowSymlinkCommand?: boolean;
     trustedDirs?: string[];
     args?: string[];
